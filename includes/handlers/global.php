d10370756d86e5813486158413f4a61878afe417
vladoja <dr.jalo2@gmail.com>
2020-08-31 10:05:06 +0200

    Global and constants.

---
+++ includes/handlers/global.php
@@ -0,0 +1,27 @@
+<?php
+
+$timezone = date_default_timezone_set("Europe/Bratislava");
+require_once './config/constants.php';
+
+function output_validation_error(string $formInputName)
+{
+    if (isset($_SESSION[ERROR_ARRAY][$formInputName]) && count($_SESSION[ERROR_ARRAY][$formInputName]) > 0) {
+        foreach ($_SESSION[ERROR_ARRAY][$formInputName] as $key => $err_message) {
+            error_log('Printing out error message: ' . $err_message);
+            echo "<br>" . $err_message;
+            echo "<hr>";
+            unset($_SESSION[ERROR_ARRAY][$formInputName][$key]);
+        }
+    }
+}
+
+// TODO: Validation error pojdu do sesssion
+function add_msg_to_errors(string $formInputName, string $error_message)
+{
+    if (!isset($_SESSION[ERROR_ARRAY])) {
+        $_SESSION[ERROR_ARRAY] = array();
+    }
+    $err_array = $_SESSION[ERROR_ARRAY][$formInputName] ?? array();
+    array_push($err_array, $error_message);
+    $_SESSION[ERROR_ARRAY][$formInputName] = $err_array;
+}
--- includes/header.php
+++ includes/header.php
@@ -1,3 +1,6 @@
+<?php
+require 'handlers/global.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
