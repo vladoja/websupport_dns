d1089d5674cc67464049f11316274da00541c915
vladoja <dr.jalo2@gmail.com>
2020-08-31 10:04:38 +0200

    Session to router script.

--- router.php
+++ router.php
@@ -1,6 +1,10 @@
 <?php
+ob_start(); // Turns on output buffering
+session_start();
+define('ERROR_ARRAY', 'error_array');
+
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-error_log('Incoming request: ' . $path);
+// error_log('Incoming request: ' . $path);
 if (file_exists($_SERVER['DOCUMENT_ROOT'] . $path)) {
     return false;
 } else {
