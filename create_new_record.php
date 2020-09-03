c16cb44b725ae8f5810bd63f9113dcf962fdb8ba
vladoja <dr.jalo2@gmail.com>
2020-09-03 10:33:17 +0200

    Styling: create new record.

--- create_new_record.php
+++ create_new_record.php
@@ -13,29 +13,29 @@ if (!$form_type) {
 }
 
 ?>
-    <h2><a href="index.php">DNS manager</a>
-    </h2>
-    <h4>Create new DNS Record</h4>
-    <div class="row">
-        <?php output_validation_error(FORM_ERROR_MESSAGES);?>
-    </div>
-    <div class="container-form">
-        <?php if (isset($form_type)){
-            echo '<div class="form-header">DNS Zaznam typu: '. strtoupper($form_type).' </div>';
-        }?>
-        <form method="POST" action="create_new_record.php">
-            <div class="form-wrapper">
-                <input type="hidden" id="dns_type" name="dns_type" value="<?php echo $form_type;?>">
-                <?php
-                    include('./includes/dns_forms/form_'.strtolower($form_type).'.php');
-                ?>
 
-                <div class="form-group">
-                    <input type="submit" name="create_button" class="form-control" value="Vytvorit">
+    <div class="container">
+        <div class="row">
+            <?php output_validation_error(FORM_ERROR_MESSAGES);?>
+        </div>
+        <div class="container-form">
+            <?php if (isset($form_type)){
+                echo '<div class="form-header">DNS Zaznam typu: '. strtoupper($form_type).' </div>';
+            }?>
+            <form method="POST" action="create_new_record.php">
+                <div class="form-wrapper">
+                    <input type="hidden" id="dns_type" name="dns_type" value="<?php echo $form_type;?>">
+                    <?php
+                        include('./includes/dns_forms/form_'.strtolower($form_type).'.php');
+                    ?>
+
+                    <div class="form-group">
+                        <input type="submit" name="create_button" class="form-control" value="Vytvorit">
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
 
+        </div>
     </div>
 </div>
 </body>
